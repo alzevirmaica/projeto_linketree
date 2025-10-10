4cdf6632b10c6e9a3ad96ae48700d9d45078a73a
Alzevir Maicá <alzevir.maica@atlastechnol.com>
2025-10-10 15:50:08 -0300

    Fetching data from the database

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,73 @@
 import { Social } from "../../components/social";
 import { FaInstagram, FaYoutube, FaLinkedin } from "react-icons/fa";
+import { useEffect, useState } from "react";
+import { db } from "../../services/firebaseConnection";
+import {
+  getDocs,
+  collection,
+  orderBy,
+  query,
+  doc,
+  getDoc,
+} from "firebase/firestore";
+
+interface LinksProps {
+  id: string;
+  name: string;
+  url: string;
+  bg: string;
+  color: string;
+}
+
+interface socialLinkksProps {
+  linkedin: string;
+  instagram: string;
+  youtube: string;
+}
 
 function Home() {
+  const [links, setLinks] = useState<LinksProps[]>([]);
+  const [sociaLinks, setSocialLinks] = useState<socialLinkksProps>();
+
+  useEffect(() => {
+    function loadLinks() {
+      const linksRef = collection(db, "links");
+      const queryRef = query(linksRef, orderBy("created", "asc"));
+
+      getDocs(queryRef).then((snapshot) => {
+        const lista = [] as LinksProps[];
+
+        snapshot.forEach((doc) => {
+          lista.push({
+            id: doc.id,
+            name: doc.data().name,
+            url: doc.data().url,
+            bg: doc.data().bg,
+            color: doc.data().color,
+          });
+        });
+        setLinks(lista);
+      });
+    }
+    loadLinks();
+  }, []);
+
+  useEffect(() => {
+    function loadSocialLinks() {
+      const docRef = doc(db, "social", "link");
+      getDoc(docRef).then((snapshot) => {
+        if (snapshot.data() !== undefined) {
+          setSocialLinks({
+            instagram: snapshot.data()?.instagram,
+            linkedin: snapshot.data()?.linkedin,
+            youtube: snapshot.data()?.youtube,
+          });
+        }
+      });
+    }
+    loadSocialLinks();
+  }, []);
+
   return (
     <div className="flex flex-col w-full py-4 items-center justify-center">
       <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">
@@ -10,23 +76,31 @@ function Home() {
       <span className="text-gray-50 mb-5 mt-3">Veja meus links 👇</span>
 
       <main className="flex flex-col w-11/12 max-w-xl text-center">
-        <section className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer">
-          <a>
-            <p className="text-base md:text-lg">Canal no Youtube</p>
-          </a>
-        </section>
+        {links.map((link) => (
+          <section
+            style={{ backgroundColor: link.bg }}
+            key={link.id}
+            className="bg-white mb-4 w-full py-2 rounded-lg select-none transition-transform hover:scale-105 cursor-pointer"
+          >
+            <a href={link.url} target="_blank" style={{ color: link.color }}>
+              <p className="text-base md:text-lg">{link.name}</p>
+            </a>
+          </section>
+        ))}
 
-        <footer className="flex justify-center gap-3 my-4">
-          <Social url="https://www.linkedin.com/in/alzevir-maica/">
-            <FaLinkedin size={35} color="#FFF" />
-          </Social>
-          <Social url="https://www.youtube.com/@alzevirmaicaoficial917">
-            <FaYoutube size={35} color="#FFF" />
-          </Social>
-          <Social url="https://www.instagram.com/alzevirmaica/">
-            <FaInstagram size={35} color="#FFF" />
-          </Social>
-        </footer>
+        {sociaLinks && Object.keys(sociaLinks).length > 0 && (
+          <footer className="flex justify-center gap-3 my-4">
+            <Social url={sociaLinks?.linkedin}>
+              <FaLinkedin size={35} color="#FFF" />
+            </Social>
+            <Social url={sociaLinks?.youtube}>
+              <FaYoutube size={35} color="#FFF" />
+            </Social>
+            <Social url={sociaLinks?.instagram}>
+              <FaInstagram size={35} color="#FFF" />
+            </Social>
+          </footer>
+        )}
       </main>
     </div>
   );
